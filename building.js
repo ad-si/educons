a414c0f35f84e45a0346625b49a74d1118bce773
adius <mail@adriansieber.com>
2014-10-08 14:52:22 +0200

    Enhanced deployment script, bugfixes

--- building.js
+++ building.js
@@ -20,7 +20,7 @@ module.exports = function (conf) {
 		['g', {
 			style: {
 				fill: 'transparent',
-				stroke: 'black',
+				stroke: conf.color,
 				'stroke-linecap': 'round',
 				'stroke-linejoin': 'round'
 			}},
@@ -29,7 +29,7 @@ module.exports = function (conf) {
 				   "L37,39 L37,81 L47,81 L47,39 L53,39 L53,81 L63,81 L63,39 " +
 				   "L69,39 L69,81 L79,81 L79,39 L85,39 L85,82 L93,89 L7,89 Z " +
 				   "M7,33 L93,33 L50,11 L7,33 Z",
-				fill: conf.outline ? false : 'black',
+				fill: conf.outline ? false : conf.color,
 				'stroke-width': 2}
 			]
 		]
